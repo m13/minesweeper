8e9f0a6e3c646f13220c671ec921898547c6ea0b
Sergio Arcos <sergio.arcos@gmail.com>
2019-07-20 17:26:09 +0900

    feat(rspec): first fake test

--- Gemfile
+++ Gemfile
@@ -5,3 +5,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
+group :test do
+  gem 'rspec'
+end
---
+++ spec/minesweeper_spec.rb
@@ -0,0 +1,10 @@
+require "#{__dir__}/../src/minesweeper"
+
+RSpec.describe Minesweeper do
+  context '#status' do
+    it 'should return status' do
+      game = Minesweeper.new(10, :easy)
+      expect { game.status }.to output(/status/).to_stdout
+    end
+  end
+end
